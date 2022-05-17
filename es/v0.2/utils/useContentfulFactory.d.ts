158956b3173abd471cbb15b87671a2ab8987c2ce
MisaGu <aslebed@yahoo.com>
2022-05-17 20:00:52 +0300

    remove unused code

--- es/v0.2/utils/useContentfulFactory.d.ts
+++
@@ -1,6 +0,0 @@
-import { Variables } from 'graphql-request';
-import { UseQueryOptions, UseQueryResult } from 'react-query';
-import { EContentfulQueries } from '../../data';
-export declare function useContentfulFactory<T = any, V = Variables>(query: EContentfulQueries, variables?: V, options?: UseQueryOptions<T>): UseQueryResult<T> & {
-    loading: boolean;
-};
